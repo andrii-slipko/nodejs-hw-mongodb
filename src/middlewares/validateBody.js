55af6705c276c6ae4ca7f3e5681c3d1d1410efb9
andrii-slipko <andreyslipko0703@gmail.com>
2025-01-30 15:01:59 +0200

    hw4 fix valid

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,14 @@
-import createHttpError from 'http-errors';
+import createHttpError from "http-errors";
 
 const validateBody = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
     if (error) {
-      return next(createHttpError(400, error.message));
+      const errorMessages = error.details.map((err) => err.message).join(", ");
+      return next(createHttpError(400, `Validation error: ${errorMessages}`));
     }
+
     next();
   };
 };
--- src/models/contactSchemas.js
+++ src/models/contactSchemas.js
@@ -3,13 +3,15 @@ import Joi from 'joi';
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  contactType: Joi.string().valid('work', 'personal', 'other').required(),
+  email: Joi.string().email().required(), 
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),  
   isFavourite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-  contactType: Joi.string().valid('work', 'personal', 'other'),
+  email: Joi.string().email(), 
+  contactType: Joi.string().valid('work', 'home', 'personal'),  
   isFavourite: Joi.boolean(),
-}).min(1);
+}).min(1);  
